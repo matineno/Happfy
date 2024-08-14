56e7dbd8baf2efeaa408b193f3422a35c856f216
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-08-14 15:11:09 -0500

    added header component and react icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 function Header() {
     return (
-          <div className =" header container flex space-between">
+          <div className ="header container flex space-between">
+            <div className ="logo-img">
+            </div>
+            
           </div>
       
     );
