6a63afafdcbeb42e4f5e8b92128fbc8615ed0836
Adogamhe Aleogho Martins(Jnr.) Andre <martinsadogamhe9@gmail.com>
2024-08-16 11:16:29 -0500

    moved pages to folder and eddited App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import TopTracks from './components/TopTracks';
-import TopRecommendations from './components/TopRecommendations';
-import Playlist from './components/Playlist';
-import SpotifyEmbed from './components/SpotifyEmbed';
 import Header from './components/Header';
-import SpotifyArtist from './components/Artists';
 import Menu from './components/Menu';
+import SpotifyEmbed from './components/SpotifyEmbed';
 
 function App() {
   return (
@@ -18,11 +14,7 @@ function App() {
             <Header />
           </header>
           <main >
-            <TopTracks />
-            <TopRecommendations />
-            <Playlist />
             <SpotifyEmbed />
-            <SpotifyArtist />
           </main>
       </div>
     </div>
